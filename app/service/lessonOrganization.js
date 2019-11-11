c9821f3c4fd5e9108784ee1f0d0cb2a17a826050
jacksparrow68 <954457807@qq.com>
2019-11-11 15:43:10 +0800

    fix bug

--- app/service/lessonOrganization.js
+++ app/service/lessonOrganization.js
@@ -204,7 +204,7 @@ class LessonOrgService extends Service {
                 where: { organizationId, packageId, classId },
             }).then(list => _.map(list, o => o.toJSON()));
         } else {
-            const classIds = await this.model.lessonOrganizationClassMember.getAllClassIds(
+            const classIds = await this.model.LessonOrganizationClassMember.getAllClassIds(
                 { memberId: userId, roleId, organizationId }
             );
             list = await this.model.LessonOrganizationPackage.findAll({
