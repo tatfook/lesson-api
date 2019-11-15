c3a974e4345c9f59bc3f2f9d615c76d981c3927d
Owen <253065903@qq.com>
2019-11-15 14:15:05 +0800

    debug

--- app/common/api.js
+++ app/common/api.js
@@ -46,6 +46,8 @@ class Api {
                 return res.data;
             })
             .catch(res => {
+                // eslint-disable-next-line no-console
+                console.log(res);
                 this.app.logger.debug(
                     `请求:${url}失败`,
                     res.responsestatus,
