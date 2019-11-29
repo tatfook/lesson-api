129124080a8725669ce8e6d6fc2c1147219c205c
jacksparrow68 <954457807@qq.com>
2019-11-29 15:07:16 +0800

    单元测试

---
+++ test/mock/factory/lessonOrganizationClass.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = app => {
+	const { factory } = app;
+	const tableName = 'LessonOrganizationClass';
+
+	factory.define(tableName, app.model[tableName], {
+		organizationId: app.factory.assoc('LessonOrganization', 'id'),
+		name: factory.chance('string', {
+			length: 5,
+		}),
+	});
+};
