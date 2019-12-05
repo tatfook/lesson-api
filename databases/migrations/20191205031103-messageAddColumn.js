6290efc9cc7f5004bad6418f954fa9331ede7891
jacksparrow68 <954457807@qq.com>
2019-12-05 11:14:07 +0800

    fix bug, #5786

---
+++ databases/migrations/20191205031103-messageAddColumn.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('messages', 'receivers', {
+      type: Sequelize.TEXT,
+      comment: '发送给的用户名,逗号隔开【系统消息only】',
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('messages', 'receivers');
+  },
+};
