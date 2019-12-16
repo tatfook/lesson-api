c17ecfd5b2dc64d585d6d9c7675f9e9c649e9539
jacksparrow68 <954457807@qq.com>
2019-12-16 16:50:05 +0800

    seeds

--- databases/seeders/20191210075005-activateCode.js
+++ databases/seeders/20191210075005-activateCode.js
@@ -6,23 +6,23 @@ module.exports = {
         try {
             const codes = await queryInterface.sequelize.query(
                 `
-        select 
-          id,
-          classId 
-        from lessonOrganizationActivateCodes 
-        where classId is not null or classId !=''
-        `,
+            select 
+              id,
+              classId 
+            from lessonOrganizationActivateCodes 
+            where classId is not null or classId !=''
+            `,
                 { type: Sequelize.QueryTypes.SELECT, transaction }
             );
             // 把classId的东西塞到classIds字段，并删除classId字段
             for (let i = 0; i < codes.length; i++) {
                 await queryInterface.sequelize.query(
                     `
-        update 
-          lessonOrganizationActivateCodes
-          set classIds = '[${codes[i].classId}]'
-        where id = ${codes[i].id}
-        `,
+            update 
+              lessonOrganizationActivateCodes
+              set classIds = '[${codes[i].classId}]'
+            where id = ${codes[i].id}
+            `,
                     { type: Sequelize.QueryTypes.UPDATE, transaction }
                 );
             }
@@ -36,13 +36,18 @@ module.exports = {
             // 把以前剩余的的激活码置为无效
             await queryInterface.sequelize.query(
                 `
-        update 
-          lessonOrganizationActivateCodes
-          set state=2
-        where state=0
-        `,
+            update 
+              lessonOrganizationActivateCodes
+              set state=2
+            where state=0
+            `,
                 { type: Sequelize.QueryTypes.UPDATE, transaction }
             );
+
+
+            await queryInterface.sequelize.query(`
+            update lessonOrganizations set activateCodeLimit = \'{\"type5\":0,\"type6\":0,\"type7\":0}\'
+            `, { type: Sequelize.QueryTypes.UPDATE, transaction })
 
             await transaction.commit();
         } catch (e) {
