2d111a941c23d25133e662fb9610a5d5e1f3971b
jacksparrow68 <954457807@qq.com>
2019-12-17 17:34:15 +0800

    unittest

--- test/controller/lessonOrganizationActivateCode.test.js
+++ test/controller/lessonOrganizationActivateCode.test.js
@@ -1,5 +1,5 @@
-const md5 = require('blueimp-md5');
 const { app, mock, assert } = require('egg-mock/bootstrap');
+const moment = require('moment');
 
 describe('机构激活码', () => {
     let token;
@@ -12,8 +12,8 @@ describe('机构激活码', () => {
         // 创建机构
         const organ = await app.model.LessonOrganization.create({
             name: 'org0000',
-            count: 1,
             endDate: new Date('2200-01-01'),
+            activateCodeLimit: { type5: 3, type6: 3, type7: 3 },
         }).then(o => o.toJSON());
         assert(organ.id);
         orgId = organ.id;
@@ -44,6 +44,7 @@ describe('机构激活码', () => {
                 .send({
                     count: 20,
                     classIds: [classId],
+                    type: 1,
                 })
                 .set('Authorization', `Bearer ${token}`)
                 .expect(200)
@@ -58,6 +59,7 @@ describe('机构激活码', () => {
                 .send({
                     count: 20,
                     classIds: [],
+                    type: 1,
                 })
                 .set('Authorization', `Bearer ${token}`)
                 .expect(200)
@@ -133,12 +135,112 @@ describe('机构激活码', () => {
         });
     });
 
-    // describe('激活码使用情况', async () => {
-    //     beforeEach(async () => {
+    describe('学生续费', async () => {
+        let key;
+        beforeEach('', async () => {
+            const ret = await app.factory.create(
+                'LessonOrganizationActivateCode',
+                {
+                    organizationId: orgId,
+                    type: 5,
+                    state: 0,
+                }
+            );
+            key = ret.key;
 
-    //     });
-    //     it('001', async () => {
+            const cls = await app.factory.create('LessonOrganizationClass', {
+                organizationId: orgId,
+                status: 1,
+            });
+            await app.model.LessonOrganizationClassMember.create({
+                organizationId: orgId,
+                memberId: 1,
+                roleId: 1,
+                classId: cls.id,
+                endTime: '2200-01-01',
+            });
+        });
+        it('001', async () => {
+            await app
+                .httpRequest()
+                .post('/lessonOrganizationActivateCodes/stuRecharge')
+                .send({
+                    key,
+                    realname: 'abc',
+                })
+                .set('Authorization', `Bearer ${token}`)
+                .expect(200)
+                .then(res => res.body);
 
-    //     });
-    // });
+            const member = await app.model.LessonOrganizationClassMember.findOne(
+                {
+                    where: {
+                        memberId: 1,
+                        organizationId: orgId,
+                        roleId: 1,
+                    },
+                }
+            );
+            assert(
+                moment(member.endTime).format('YYYY-MM-DD') === '2200-04-01'
+            );
+        });
+    });
+
+    describe('激活码使用情况', async () => {
+        beforeEach(async () => {
+            await app.model.LessonOrganizationActivateCode.create({
+                organizationId: orgId,
+                type: 5,
+                key: 'a',
+                state: 1,
+            });
+        });
+        it('001', async () => {
+            const ret = await app
+                .httpRequest()
+                .get('/lessonOrganizationActivateCodes/usedStatus')
+                .set('Authorization', `Bearer ${token}`)
+                .expect(200)
+                .then(res => res.body.data);
+            assert(
+                ret.remainder.type5 === 2 &&
+                    ret.remainder.type6 === 3 &&
+                    ret.remainder.type7 === 3 &&
+                    ret.used.type5 === 1
+            );
+        });
+    });
+
+    describe('激活码设为无效', async () => {
+        beforeEach(async () => {
+            await app.model.LessonOrganizationActivateCode.create({
+                organizationId: orgId,
+                type: 5,
+                key: 'a',
+                state: 0,
+            });
+        });
+        it('001', async () => {
+            const ret = await app
+                .httpRequest()
+                .put('/lessonOrganizationActivateCodes/invalid')
+                .send({
+                    ids: [1],
+                })
+                .set('Authorization', `Bearer ${token}`)
+                .expect(200)
+                .then(res => res.body.data);
+        });
+        it('002', async () => {
+            const ret = await app
+                .httpRequest()
+                .put('/lessonOrganizationActivateCodes/invalid')
+                .send({
+                    ids: [0],
+                })
+                .set('Authorization', `Bearer ${token}`)
+                .expect(422);
+        });
+    });
 });
