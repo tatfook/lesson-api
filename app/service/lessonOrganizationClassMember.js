21040fe1fc943fef96ae671d33f6a103a366f27c
jacksparrow68 <954457807@qq.com>
2019-12-20 15:09:46 +0800

    fix bug

--- app/service/lessonOrganizationClassMember.js
+++ app/service/lessonOrganizationClassMember.js
@@ -17,8 +17,8 @@ const {
 const Err = require('../common/err');
 const _ = require('lodash');
 const moment = require('moment');
-const formalTypes = [ '5', '6', '7' ]; // 正式邀请码类型
-const allCodeTypes = [ '1', '2', '5', '6', '7' ]; // 全部邀请码类型
+const formalTypes = ['5', '6', '7']; // 正式邀请码类型
+const allCodeTypes = ['1', '2', '5', '6', '7']; // 全部邀请码类型
 
 // 各个类型激活码的过期时间
 const endTimeMap = {
@@ -202,7 +202,7 @@ class LessonOrgClassMemberService extends Service {
         const memberIds = members.map(o => o.memberId);
         if (memberIds.length === 0) return { count: 0, rows: [] };
 
-        const [ list, users ] = await Promise.all([
+        const [list, users] = await Promise.all([
             this.model.LessonOrganizationClassMember.findAll({
                 include: [
                     {
@@ -315,7 +315,7 @@ class LessonOrgClassMemberService extends Service {
 
         const oldmembers = await this.ctx.model.LessonOrganizationClassMember.findAll(
             {
-                order: [[ 'id', 'desc' ]],
+                order: [['id', 'desc']],
                 include: [
                     {
                         as: 'lessonOrganizationClasses',
@@ -432,11 +432,13 @@ class LessonOrgClassMemberService extends Service {
 
         if (oldmembers.length) {
             // 不要丢失用户类型，到期时间，家长手机号
-            const old = oldmembers[0];
+            const type = (_.find(oldmembers, o => o.type) || {}).type;
+            const endTime = (_.find(oldmembers, o => o.endTime) || {}).endTime;
+            const parentPhoneNum = (_.find(oldmembers, o => o.parentPhoneNum) || {}).parentPhoneNum;
             datas.forEach(r => {
-                r.type = old.type;
-                r.endTime = old.endTime;
-                r.parentPhoneNum = old.parentPhoneNum;
+                r.type = type;
+                r.endTime = endTime;
+                r.parentPhoneNum = parentPhoneNum;
             });
         }
 
@@ -540,7 +542,7 @@ class LessonOrgClassMemberService extends Service {
         if (~~params.roleId & CLASS_MEMBER_ROLE_STUDENT) {
             await this.ctx.service.evaluationReport.checkEvaluationStatus(
                 member.memberId,
-                [ member.classId ]
+                [member.classId]
             );
         }
 
@@ -558,8 +560,8 @@ class LessonOrgClassMemberService extends Service {
             organizationId,
             handleId: userId,
             username,
-            oldmembers: [ member ],
-            classIds: [ -1 ],
+            oldmembers: [member],
+            classIds: [-1],
             roleId:
                 memberRoleId & CLASS_MEMBER_ROLE_TEACHER
                     ? CLASS_MEMBER_ROLE_TEACHER
@@ -627,7 +629,7 @@ class LessonOrgClassMemberService extends Service {
         if (!formalTypes.includes(type + '')) {
             this.ctx.throw(400, Err.STU_TYPE_ERR);
         }
-        const [ members, classes, org, historyCount ] = await Promise.all([
+        const [members, classes, org, historyCount] = await Promise.all([
             //
             this.ctx.model.LessonOrganizationClassMember.findAll({
                 where: {
@@ -656,7 +658,7 @@ class LessonOrgClassMemberService extends Service {
                     organizationId,
                     type,
                     state: {
-                        $in: [ '0', '1' ],
+                        $in: ['0', '1'],
                     },
                 }
             ),
@@ -692,7 +694,7 @@ class LessonOrgClassMemberService extends Service {
                 activateTime: currTime,
                 key: `${
                     classIds ? classIds.reduce((p, c) => p + c, '') : ''
-                }${i}${currTime.getTime()}${_.random(TEN, NINTYNINE)}`,
+                    }${i}${currTime.getTime()}${_.random(TEN, NINTYNINE)}`,
                 name: '',
             });
         }
@@ -852,7 +854,7 @@ class LessonOrgClassMemberService extends Service {
             this.ctx.throw(400, Err.STU_TYPE_ERR);
         }
         const currTime = new Date();
-        const [ members, classes, org, historyCount ] = await Promise.all([
+        const [members, classes, org, historyCount] = await Promise.all([
             // 检查这些学生是不是在这个机构正式学生
             this.ctx.model.LessonOrganizationClassMember.findAll({
                 where: {
@@ -882,7 +884,7 @@ class LessonOrgClassMemberService extends Service {
                     organizationId,
                     type,
                     state: {
-                        $in: [ '0', '1' ],
+                        $in: ['0', '1'],
                     },
                 }
             ),
@@ -917,7 +919,7 @@ class LessonOrgClassMemberService extends Service {
                 activateTime: currTime,
                 key: `${
                     classIds ? classIds.reduce((p, c) => p + c, '') : ''
-                }${i}${currTime.getTime()}${_.random(TEN, NINTYNINE)}`,
+                    }${i}${currTime.getTime()}${_.random(TEN, NINTYNINE)}`,
                 name: '',
             });
         }
@@ -1081,7 +1083,7 @@ class LessonOrgClassMemberService extends Service {
         }
 
         const currTime = new Date();
-        const [ members, classes, org, historyCount ] = await Promise.all([
+        const [members, classes, org, historyCount] = await Promise.all([
             // 检查这些学生是不是过期了
             this.ctx.model.LessonOrganizationClassMember.findAll({
                 where: {
@@ -1110,7 +1112,7 @@ class LessonOrgClassMemberService extends Service {
                     organizationId,
                     type,
                     state: {
-                        $in: [ '0', '1' ],
+                        $in: ['0', '1'],
                     },
                 }
             ),
@@ -1145,7 +1147,7 @@ class LessonOrgClassMemberService extends Service {
                 activateTime: currTime,
                 key: `${
                     classIds ? classIds.reduce((p, c) => p + c, '') : ''
-                }${i}${currTime.getTime()}${_.random(TEN, NINTYNINE)}`,
+                    }${i}${currTime.getTime()}${_.random(TEN, NINTYNINE)}`,
                 name: '',
             });
         }
