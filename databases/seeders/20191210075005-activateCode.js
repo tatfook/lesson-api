bbd460fbcd2d6900c84c9d839e5594f3ed284ea2
jacksparrow68 <954457807@qq.com>
2019-12-20 11:04:24 +0800

    migration change

--- databases/seeders/20191210075005-activateCode.js
+++ databases/seeders/20191210075005-activateCode.js
@@ -68,7 +68,7 @@ module.exports = {
                     `
                 update lessonOrganizationClasses set \`status\` = ${
                     classes[i].end > currTime ? 1 : 2
-                } where id = ${classes[i].id}
+                    } where id = ${classes[i].id}
                 `,
                     { type: Sequelize.QueryTypes.UPDATE, transaction }
                 );
@@ -82,6 +82,16 @@ module.exports = {
                 'lessonOrganizationClasses',
                 'end',
                 { transaction }
+            );
+
+            await queryInterface.sequelize.query(
+                `
+                update lessonOrganizationClassMembers m 
+                set m.endTime = (
+                    select endDate from lessonOrganizations where id = m.organizationId
+                ) where id >0;
+            `,
+                { type: Sequelize.QueryTypes.UPDATE, transaction }
             );
 
             await transaction.commit();
