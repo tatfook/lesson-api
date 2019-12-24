8206bc94e92787cfd53bb32ab0e2703a1f50cd87
jacksparrow68 <954457807@qq.com>
2019-12-24 14:00:57 +0800

    service层机构的测试

---
+++ test/mock/factory/lessonOrganizationPackage.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = app => {
+	const { factory } = app;
+	const tableName = 'LessonOrganizationPackage';
+
+	factory.define(tableName, app.model[tableName], {
+		organizationId: app.factory.assoc('LessonOrganization', 'id'),
+		classId: app.factory.assoc('LessonOrganizationClass', 'id'),
+		packageId: app.factory.assoc('Package', 'id'),
+		lessons: [],
+	});
+};
