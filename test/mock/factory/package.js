abb0d20a58c8521211fc2530b638c9efa64f937a
jacksparrow68 <954457807@qq.com>
2019-12-24 14:01:27 +0800

    format

--- test/mock/factory/package.js
+++ test/mock/factory/package.js
@@ -1,10 +1,10 @@
 module.exports = app => {
-	const { factory } = app;
-	const tableName = 'Package';
+    const { factory } = app;
+    const tableName = 'Package';
 
-	factory.define(tableName, app.model[tableName], {
-		userId: factory.chance('integer', { min: 1 }),
-		packageName: factory.chance('string', { length: 5 }),
-		subjectId: factory.chance('integer', { min: 1 }),
-	});
+    factory.define(tableName, app.model[tableName], {
+        userId: factory.chance('integer', { min: 1 }),
+        packageName: factory.chance('string', { length: 5 }),
+        subjectId: factory.chance('integer', { min: 1 }),
+    });
 };
