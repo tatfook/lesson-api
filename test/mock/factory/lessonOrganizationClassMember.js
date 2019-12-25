d6da1b7dd446d16756b48b3755ea7b0c0512be48
jacksparrow68 <954457807@qq.com>
2019-12-25 15:15:01 +0800

    format

--- test/mock/factory/lessonOrganizationClassMember.js
+++ test/mock/factory/lessonOrganizationClassMember.js
@@ -12,10 +12,10 @@ module.exports = app => {
         memberId: app.factory.assoc('User', 'id'),
         roleId:
             roleIds[
-            app.factory.chance('integer', {
-                min: 0,
-                max: 6,
-            })
+                app.factory.chance('integer', {
+                    min: 0,
+                    max: 6,
+                })
             ],
         realname: app.factory.chance('word', { length: 6 }),
         type: app.factory.chance('integer', { min: 1, max: 2 }),
