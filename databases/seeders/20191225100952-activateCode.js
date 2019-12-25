0bc2c54748c1860324ad0bb36aca99d247d4fa0b
jacksparrow68 <954457807@qq.com>
2019-12-25 18:19:03 +0800

    format

--- databases/seeders/20191225100952-activateCode.js
+++ databases/seeders/20191225100952-activateCode.js
@@ -1,60 +1,60 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    const transaction = await queryInterface.sequelize.transaction();
-    try {
-      // 确定班级的status, 然后删除end,begin
-      const classes = await queryInterface.sequelize.query(
-        `
+    up: async (queryInterface, Sequelize) => {
+        const transaction = await queryInterface.sequelize.transaction();
+        try {
+            // 确定班级的status, 然后删除end,begin
+            const classes = await queryInterface.sequelize.query(
+                `
             select 
               id,
               begin,
               end
             from lessonOrganizationClasses 
             `,
-        { type: Sequelize.QueryTypes.SELECT, transaction }
-      );
-      const currTime = new Date();
-      for (let i = 0; i < classes.length; i++) {
-        await queryInterface.sequelize.query(
-          `
+                { type: Sequelize.QueryTypes.SELECT, transaction }
+            );
+            const currTime = new Date();
+            for (let i = 0; i < classes.length; i++) {
+                await queryInterface.sequelize.query(
+                    `
                 update lessonOrganizationClasses set \`status\` = ${
-          classes[i].end > currTime ? 1 : 2
-          } where id = ${classes[i].id}
+                    classes[i].end > currTime ? 1 : 2
+                } where id = ${classes[i].id}
                 `,
-          { type: Sequelize.QueryTypes.UPDATE, transaction }
-        );
-      }
-      await queryInterface.removeColumn(
-        'lessonOrganizationClasses',
-        'begin',
-        { transaction }
-      );
-      await queryInterface.removeColumn(
-        'lessonOrganizationClasses',
-        'end',
-        { transaction }
-      );
+                    { type: Sequelize.QueryTypes.UPDATE, transaction }
+                );
+            }
+            await queryInterface.removeColumn(
+                'lessonOrganizationClasses',
+                'begin',
+                { transaction }
+            );
+            await queryInterface.removeColumn(
+                'lessonOrganizationClasses',
+                'end',
+                { transaction }
+            );
 
-      await queryInterface.sequelize.query(
-        `
+            await queryInterface.sequelize.query(
+                `
                 update lessonOrganizationClassMembers m 
                 set type=2,m.endTime = (
                     select endDate from lessonOrganizations where id = m.organizationId
                 ) where id >0;
             `,
-        { type: Sequelize.QueryTypes.UPDATE, transaction }
-      );
+                { type: Sequelize.QueryTypes.UPDATE, transaction }
+            );
 
-      await transaction.commit();
-    } catch (e) {
-      await transaction.rollback();
-      throw e;
-    }
-  },
+            await transaction.commit();
+        } catch (e) {
+            await transaction.rollback();
+            throw e;
+        }
+    },
 
-  down: (queryInterface, Sequelize) => {
-    //
-  },
+    down: (queryInterface, Sequelize) => {
+        //
+    },
 };
