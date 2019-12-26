bb02b503f6a1cbf08778d96ab9aafa0fe67a3395
jacksparrow68 <954457807@qq.com>
2019-12-26 11:00:18 +0800

    format

--- app/validator/lessonOrganization/organizationIdsRule.js
+++ app/validator/lessonOrganization/organizationIdsRule.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-	type: 'object',
-	properties: {
-		organizationIds: {
-			type: 'array',
-			items: {
-				type: 'number',
-				minimum: 1,
-			},
-		},
-	},
+    type: 'object',
+    properties: {
+        organizationIds: {
+            type: 'array',
+            items: {
+                type: 'number',
+                minimum: 1,
+            },
+        },
+    },
 };
