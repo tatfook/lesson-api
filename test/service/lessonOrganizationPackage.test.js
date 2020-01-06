1ce567277eca53adfd155ef6fdc6cd3adafa9cc6
jacksparrow68 <954457807@qq.com>
2020-01-06 19:51:20 +0800

    format

--- test/service/lessonOrganizationPackage.test.js
+++ test/service/lessonOrganizationPackage.test.js
@@ -3,22 +3,23 @@ const _ = require('lodash');
 const moment = require('moment');
 
 describe('test/service/lessonOrganizationPackage.test.js', async () => {
-	describe('destroyByCondition', async () => {
-		let pkgs;
-		beforeEach(async () => {
-			pkgs = await app.factory.createMany('LessonOrganizationPackage', 10)
-		});
-		it.only('001', async () => {
-			const ctx = app.mockContext();
-			await ctx.service.lessonOrganizationPackage.destroyByCondition({ id: pkgs[0].id });
-		});
-	});
+    describe('destroyByCondition', async () => {
+        let pkgs;
+        beforeEach(async () => {
+            pkgs = await app.factory.createMany(
+                'LessonOrganizationPackage',
+                10
+            );
+        });
+        it.only('001', async () => {
+            const ctx = app.mockContext();
+            await ctx.service.lessonOrganizationPackage.destroyByCondition({
+                id: pkgs[0].id,
+            });
+        });
+    });
 
-	describe('findAllByCondition', async () => {
+    describe('findAllByCondition', async () => {});
 
-	});
-
-	describe('findAllAndExtraByCondition', async () => {
-
-	});
+    describe('findAllAndExtraByCondition', async () => {});
 });
