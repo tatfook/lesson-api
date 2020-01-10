e7442d3ffe56f8d69c789f8e303a93dc524f90ed
jacksparrow68 <954457807@qq.com>
2020-01-10 10:50:28 +0800

    删除上次迭代的多余字段

---
+++ databases/migrations/20200110023125-removeColumns.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+       return await Promise.all([
+        queryInterface.removeColumn(
+          'lessonOrganizationClasses',
+          'begin',
+        ),
+        queryInterface.removeColumn(
+          'lessonOrganizationClasses',
+          'end',
+        ),
+        queryInterface.removeColumn(
+          'lessonOrganizationActivateCodes',
+          'classId',
+        )
+       ]);
+    },
+
+    down: (queryInterface, Sequelize) => {
+       // do nothing
+    },
+};
