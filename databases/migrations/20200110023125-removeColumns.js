d86a37edbef6b81930e5b8053be715af256212ab
jacksparrow68 <954457807@qq.com>
2020-01-10 10:51:01 +0800

    format

--- databases/migrations/20200110023125-removeColumns.js
+++ databases/migrations/20200110023125-removeColumns.js
@@ -2,23 +2,17 @@
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-       return await Promise.all([
-        queryInterface.removeColumn(
-          'lessonOrganizationClasses',
-          'begin',
-        ),
-        queryInterface.removeColumn(
-          'lessonOrganizationClasses',
-          'end',
-        ),
-        queryInterface.removeColumn(
-          'lessonOrganizationActivateCodes',
-          'classId',
-        )
-       ]);
+        return await Promise.all([
+            queryInterface.removeColumn('lessonOrganizationClasses', 'begin'),
+            queryInterface.removeColumn('lessonOrganizationClasses', 'end'),
+            queryInterface.removeColumn(
+                'lessonOrganizationActivateCodes',
+                'classId'
+            ),
+        ]);
     },
 
     down: (queryInterface, Sequelize) => {
-       // do nothing
+        // do nothing
     },
 };
